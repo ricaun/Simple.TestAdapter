6e2546f845cfc6f75f19e1e8f7a1d04d601f4e21
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2023-10-11 17:49:26 -0300

    Add `xunit` Test

--- TestProject.Tests/Tests.cs
+++ TestProject.Tests/Tests.cs
@@ -2,7 +2,7 @@ using NUnit.Framework;
 
 namespace TestProject.Tests
 {
-    public class TestAttribute : System.Attribute { }
+    //public class TestAttribute : System.Attribute { }
     public class Tests
     {
         [Test]
---
+++ TestProject.Tests/Tests_Xunit.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace TestProject.Tests
+{
+    //public class FactAttribute : System.Attribute { }
+    public class Tests_Xunit
+    {
+        [Fact]
+        public void Test()
+        {
+
+        }
+    }
+}
